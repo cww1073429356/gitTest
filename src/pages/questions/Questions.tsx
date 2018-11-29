8fe3bbdac89b142d734b93b5b2beca8b1419dcdd
jinjinwen <274880230@qq.com>
2018-11-29 16:18:12 +0800

    'modify'

--- src/pages/questions/Questions.tsx
+++ src/pages/questions/Questions.tsx
@@ -4,7 +4,8 @@ export default class Questions extends Component{
     render(){
         return (
             <div className="page" id="questions">
-                <h1>问答</h1>
+                {/* <h1>问答</h1> */}
+                <h3>这是gitTest</h3>
             </div>
         )
     }
